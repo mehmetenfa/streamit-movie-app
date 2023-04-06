81a01654aae6ace2aafada8b10db75fa175b70ee
qxan <mehmet.enfa@gmail.com>
2023-04-06 08:34:51 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HomePage from './home/HomePage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/header/Header'
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
         </Switch>
--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { latest, recommended, upcome } from '../dummyData'
 
 const HomePage = () => {
   return (

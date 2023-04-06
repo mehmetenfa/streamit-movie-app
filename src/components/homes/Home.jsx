0d2ead05081d66f37f309bb70ffecb3f7b831ef3
qxan <mehmet.enfa@gmail.com>
2023-04-06 09:13:33 +0300

    home

--- src/components/homes/Home.jsx
+++ src/components/homes/Home.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import HomeCard from './HomeCard'
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import HomeCard from "./HomeCard";
 
-
-const Home = () => {
+const SampleNextArrow = (props) => {
+  const { onClick } = props;
   return (
-    <div>Home</div>
-  )
-}
+    <div className="control-btn" onClick={onClick}>
+      <button className="next">
+        <i className="fa fa-chevron-right"></i>
+      </button>
+    </div>
+  );
+};
+const SamplePrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="control-btn" onClick={onClick}>
+      <button className="prev">
+        <i className="fa fa-chevron-left"></i>
+      </button>
+    </div>
+  );
+};
 
-export default Home
+const Home = ({ items }) => {
+  const settings = {
+    dots: false,
+    infitinte: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <>
+      <div className="homeContainer">
+        <Slider {...settings}>
+          {items.map((item) => {
+            return (
+              <>
+                <HomeCard key={item.id} item={item} />
+              </>
+            );
+          })}
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default Home;

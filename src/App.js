06cc51a9c9f8d4b51678530a98a5623f0c9654e2
qxan <mehmet.enfa@gmail.com>
2023-04-07 06:04:35 +0300

    appjs singlepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HomePage from './home/HomePage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/header/Header'
+import SinglePage from './components/watch/SinglePage'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
+          <Route exact path='/singlepage/:id' component={SinglePage} />
         </Switch>
       </Router>
     </>

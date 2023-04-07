057a93918ddecf3a8aec7cbef8aacd694974e5cb
qxan <mehmet.enfa@gmail.com>
2023-04-07 05:50:21 +0300

    trending

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
-import { latest, recommended, upcome } from '../dummyData'
-import Homes from '../components/homes/Homes'
-import Upcomming from '../components/upcomming/Upcomming'
- 
+import React, { useState } from "react";
+import Homes from "../components/homes/Homes";
+import Trending from "../components/trending/Trending";
+import Upcomming from "../components/upcoming/Upcomming";
+import { latest, recommended, upcome } from "../dummyData";
 
 const HomePage = () => {
-  const [items, setItems] = useState(upcome)
-  const [item, setItem] = useState(latest)
+  const [items, setItems] = useState(upcome);
+  const [item, setItem] = useState(latest);
+  const [rec, setRec] = useState(recommended);
   return (
     <>
       <Homes />
-      <Upcomming items={items} title='Upcomming Movies' />
-      <Upcomming items={item} title='Latest Movies' />
+      <Upcomming items={items} title="Upcomming Movies" />
+      <Upcomming items={item} title="Latest Movies" />
+      <Trending />
+      <Upcomming items={rec} title="Recommended Movies" />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;

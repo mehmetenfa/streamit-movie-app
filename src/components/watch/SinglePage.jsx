2dafa69538fa5b6a97295b7f3163d95fe39b15cd
qxan <mehmet.enfa@gmail.com>
2023-04-07 10:35:28 +0300

    singlePage

--- src/components/watch/SinglePage.jsx
+++ src/components/watch/SinglePage.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { useParams } from "react-router-dom";
+import { homeData, recommended } from "../../dummyData";
+import Upcomming from "../upcomming/Upcomming";
 
 const SinglePage = () => {
-  return (
-    <div>SinglePage</div>
-  )
-}
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
 
-export default SinglePage
+  useEffect(() => {
+    let item = homeData.find((item) => item.id === parseInt(id));
+    if (item) {
+      setItem(item);
+    }
+  }, [id]);
+  const [rec, setRec] = useState(recommended);
+  return (
+    <>
+      {item ? (
+        <>
+          <section className="singlePage">
+            <div className="singleHeading">
+              <h1>{item.name} </h1> <span> | {item.time} | </span>{" "}
+              <span> HD </span>
+            </div>
+            <div className="container">
+              <h3>Date : {item.date}</h3>
+              <p>{item.desc}</p>
+              <p>
+                Get access to the direct Project Overview with this report. Just
+                by a quick glance, you’ll have an idea of the total tasks
+                allotted to the team, number of milestones given & completed,
+                total Links created for the project and the total time taken by
+                all the users. Each section would elaborate the data further, if
+                chosen.
+              </p>
+              <p>
+                Get access to the direct Project Overview with this report. Just
+                by a quick glance, you’ll have an idea of the total tasks
+                allotted to the team, number of milestones given & completed,
+                total Links created for the project and the total time taken by
+                all the users. Each section would elaborate the data further, if
+                chosen.
+              </p>
+              <p>
+                Get access to the direct Project Overview with this report. Just
+                by a quick glance, you’ll have an idea of the total tasks
+                allotted to the team, number of milestones given & completed,
+                total Links created for the project and the total time taken by
+                all the users. Each section would elaborate the data further, if
+                chosen.
+              </p>
+            </div>
+            <div className="social">
+              <h3>Share : </h3>
+              <img src="https://img.icons8.com/color/48/000000/facebook-new.png" />
+              <img src="https://img.icons8.com/fluency/48/000000/twitter-circled.png" />
+              <img src="https://img.icons8.com/fluency/48/000000/linkedin-circled.png" />
+            </div>
+          </section>
+          <Upcomming item={rec} title="Recommended Movies" />
+        </>
+      ) : (
+        "no"
+      )}
+    </>
+  );
+};
+
+export default SinglePage;
